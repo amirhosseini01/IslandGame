2981c77a44934a5b81d1426f97c1e3a092589c69
Amir <amircsharp7@gmail.com>
2023-11-27 23:03:55 +0330

    add animation attack

--- Assets/Scripts/Characters/Combat.cs
+++ Assets/Scripts/Characters/Combat.cs
@@ -1,8 +1,33 @@
 using System;
+using Assets.Scripts.Utilities;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Combat : MonoBehaviour
 {
     [NonSerialized]
     public float Damage = 0f;
+    private Animator _animatorComponent;
+
+    public void HandleAttack(InputAction.CallbackContext context)
+    {
+        if(!context.performed)
+        {
+            return;
+        }
+
+        StartAttack();
+    }
+
+    private void Awake() 
+    {
+        _animatorComponent = GetComponentInChildren<Animator>();
+    }
+
+    private void StartAttack() 
+    {
+        _animatorComponent.SetFloat(Constants.SpeedAnimatorParam, 0);
+        _animatorComponent.SetTrigger(Constants.AttackAnimatorParam);
+
+    }
 }
--- Assets/Scripts/Utilities/Constants.cs
+++ Assets/Scripts/Utilities/Constants.cs
@@ -7,6 +7,7 @@ namespace Assets.Scripts.Utilities
         public const string PlayerTag = "Player"; 
         public const string EnemyTag = "Enemy"; 
         public const string SpeedAnimatorParam = "Speed";
+        public const string AttackAnimatorParam = "Attack";
         public const string IsShakingAnimatorParam = "IsShaking";
 
     }
