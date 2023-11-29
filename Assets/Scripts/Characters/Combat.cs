384993dbdc142972e199b64c5dae6b56ac85e111
Amir <amircsharp7@gmail.com>
2023-11-29 22:56:53 +0330

    fix: animation attack and create handle bubble hit

--- Assets/Scripts/Characters/Combat.cs
+++ Assets/Scripts/Characters/Combat.cs
@@ -7,6 +7,10 @@ public class Combat : MonoBehaviour
 {
     [NonSerialized]
     public float Damage = 0f;
+
+    [NonSerialized]
+    public bool IsAttacking = false;
+
     private Animator _animatorComponent;
     private BubbleEvent _bubbleEventComponent;
 
@@ -30,10 +34,23 @@ public class Combat : MonoBehaviour
     {
         _bubbleEventComponent.OnBubbleStartAttack += HandleBubbleStartAttack;
         _bubbleEventComponent.OnBubbleCompleteAttack += HandleBubbleCompleteAttack;
+        _bubbleEventComponent.OnBubbleHit += HandleBubbleHit;
+    }
+
+    private void OnDisable()
+    {
+        _bubbleEventComponent.OnBubbleStartAttack -= HandleBubbleStartAttack;
+        _bubbleEventComponent.OnBubbleCompleteAttack -= HandleBubbleCompleteAttack;
+        _bubbleEventComponent.OnBubbleHit -= HandleBubbleHit;
     }
 
     private void StartAttack()
     {
+        if (IsAttacking)
+        {
+            return;
+        }
+
         _animatorComponent.SetFloat(Constants.SpeedAnimatorParam, 0);
         _animatorComponent.SetTrigger(Constants.AttackAnimatorParam);
 
@@ -41,11 +58,42 @@ public class Combat : MonoBehaviour
 
     private void HandleBubbleStartAttack()
     {
-
+        IsAttacking = true;
     }
 
     private void HandleBubbleCompleteAttack()
     {
+        IsAttacking = false;
+    }
 
+    private void HandleBubbleHit()
+    {
+        #pragma warning disable UNT0028
+        var targets = Physics.BoxCastAll(
+            center: transform.position + transform.forward,
+            halfExtents: transform.localScale / 2,
+            direction: transform.forward,
+            orientation: transform.rotation,
+            maxDistance: 1f
+        );
+        #pragma warning restore UNT0028
+
+        foreach (var target in targets)
+        {
+            if (CompareTag(target.transform.tag))
+            {
+                continue;
+            }
+
+            var healthComponent = target.transform
+                .gameObject.GetComponent<Health>();
+
+            if (healthComponent is null)
+            {
+                return;
+            }
+
+            // healthComponent.TakeDamage(Damage);
+        }
     }
 }
--- Assets/Scripts/Utilities/BubbleEvent.cs
+++ Assets/Scripts/Utilities/BubbleEvent.cs
@@ -3,19 +3,24 @@ using UnityEngine.Events;
 
 namespace Assets.Scripts.Utilities
 {
-	public class BubbleEvent : MonoBehaviour
+    public class BubbleEvent : MonoBehaviour
     {
-        public event UnityAction OnBubbleStartAttack = () => {};
-        public event UnityAction OnBubbleCompleteAttack = () => {};
+        public event UnityAction OnBubbleStartAttack = () => { };
+        public event UnityAction OnBubbleCompleteAttack = () => { };
+        public event UnityAction OnBubbleHit = () => { };
         private void OnStartAttack()
         {
             OnBubbleStartAttack.Invoke();
         }
 
-
         private void OnCompleteAttack()
         {
             OnBubbleCompleteAttack.Invoke();
+        }
+
+        private void OnHit()
+        {
+            OnBubbleHit.Invoke();
         }
     }
 }
