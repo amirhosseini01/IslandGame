5206ead48b25354fe4fdabfea81a4d02a2f1d709
Amir <amircsharp7@gmail.com>
2023-12-05 22:08:42 +0330

    feat: adding & listening input event for ui scene

--- Assets/Scripts/Ui/UiController.cs
+++ Assets/Scripts/Ui/UiController.cs
@@ -1,14 +1,37 @@
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 namespace Assets.Scripts.Ui
 {
-	public class UiController : MonoBehaviour
+    public class UiController : MonoBehaviour
     {
         public UiBaseState CurrentState;
         public UiMainMenuState UiMainMenuState;
+        public List<Button> Buttons;
+        public VisualElement Root;
+        public int CurrentSelection = 0;
+
+        private UIDocument _uiDocumentComponent;
+
+        public void HandleInteract(InputAction.CallbackContext context)
+        {
+            if(!context.performed)
+            {
+                return;
+            }
+
+            CurrentState.SelectButton();
+        }
+
         private void Awake()
         {
             UiMainMenuState = new(this);
+
+            _uiDocumentComponent = GetComponent<UIDocument>();
+            Root = _uiDocumentComponent.rootVisualElement;
         }
         private void Start()
         {
--- Assets/Scripts/Ui/UiMainMenuState.cs
+++ Assets/Scripts/Ui/UiMainMenuState.cs
@@ -1,12 +1,20 @@
+using UnityEngine.UIElements;
+
 namespace Assets.Scripts.Ui
 {
 	public class UiMainMenuState : UiBaseState
 	{
-        public UiMainMenuState(UiController uiController): base(uiController)
-        {
-            
-        }
-		public override void EnterState() => throw new System.NotImplementedException();
-		public override void SelectButton() => throw new System.NotImplementedException();
+		public UiMainMenuState(UiController uiController) : base(uiController)
+		{
+
+		}
+		public override void EnterState()
+		{
+			UiController.Buttons = UiController.Root.Query<Button>(null, "menu-button").ToList();
+		}
+		public override void SelectButton()
+		{
+			// var btn = UiController.Buttons[UiController.CurrentSelection];
+		}
 	}
 }
